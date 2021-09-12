ff35cabd9529add02400ce02cc2eec7dced0d0b4
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-09-12 19:32:56 +0300

    Initial commit

---
+++ src/main/java/org/example/http/app/Main.java
@@ -0,0 +1,33 @@
+package org.example.http.app;
+
+import lombok.extern.java.Log;
+import org.example.http.framework.Server;
+import org.example.http.framework.resolver.argument.RequestHandlerMethodArgumentResolver;
+import org.example.http.framework.resolver.argument.RequestHeaderHandlerMethodArgumentResolver;
+import org.example.http.framework.resolver.argument.ResponseHandlerMethodArgumentResolver;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+@Log
+public class Main {
+  public static void main(String[] args) {
+    final var server = new Server();
+    server.autoRegisterHandlers("org.example.http.app");
+    server.addArgumentResolver(
+        new RequestHandlerMethodArgumentResolver(),
+        new ResponseHandlerMethodArgumentResolver(),
+        new RequestHeaderHandlerMethodArgumentResolver()
+    );
+//    new Thread(() -> {
+//        try {
+//            Thread.sleep(1000);
+//            server.stop();
+//            log.log(Level.INFO, "server stopped");
+//        } catch (InterruptedException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }).start();
+        server.listen(9999);
+    }
+}
